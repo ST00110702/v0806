0c595d6cc431560b0010672719204c08363bc894
ST0011 <st.game.program@gmail.com>
2020-08-06 12:18:23 +0900

    マウスの座標・中央に表示

--- v0806/Form1.cs
+++ v0806/Form1.cs
@@ -23,6 +23,14 @@ namespace v0806
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Point mp = MousePosition;
+            mp = PointToClient(mp);
+          label2.Left=mp.X;
+          label2.Top=mp.Y;
+          label2.Text = "" + mp.X + "," + mp.Y;
+          label2.Left = mp.X - label2.Width / 2;
+          label2.Top = mp.Y - label2.Height / 2;
+
 
           label1.Left += vx;
           label1.Top += vy;
